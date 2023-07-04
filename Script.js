aa08beb50ca086bc5dbde4f5da6a00e7316d732e
Rishabhpandey94 <128134757+Rishabhpandey94@users.noreply.github.com>
2023-07-05 01:11:02 +0530

    First commit

---
+++ Script.js
@@ -0,0 +1,176 @@
+import Ball from "./Balls.js";
+import Paddle from "./Paddles.js";
+
+const ballElement = document.getElementById("ball");
+const paddleTopElement = document.getElementsByClassName("paddle")[0];
+const paddleBottomElement = document.getElementsByClassName("paddle")[1];
+const paddleTopScoreElement = document.getElementById("rod1");
+const paddleBottomScoreElement = document.getElementById("rod2");
+const ball = new Ball(ballElement);
+const paddleTop = new Paddle(paddleTopElement);
+const paddleBottom = new Paddle(paddleBottomElement);
+
+ball.score = [paddleTopScoreElement, paddleBottomScoreElement];
+
+let maxScore;
+let maxRod;
+let whoWon = "";
+let windowHeight = window.innerHeight;
+let score = 0;
+
+
+//Welcome Function//
+(function () {
+	maxScore = localStorage.getItem("maxScore");
+	maxRod = localStorage.getItem("maxRod");
+	if (maxRod === null || maxScore === null) {
+		alert(
+			"This is the first time you are playing this game.\nLET'S START !!!"
+		);
+		maxScore = 0;
+		maxRod = "Rod 1";
+		return;
+	} else {
+		alert(`${maxRod} has maximum score of ${maxScore}`);
+		return;
+	}
+})();
+
+let previousTime;
+let clearFrame,
+	clearFrame2,
+	animation = true;
+let updateAnimationFrame = (currentTime) => {
+	if (previousTime != null) {
+		//Δ Negligible amount of Time
+		const delta = currentTime - previousTime;
+		//Calling another animation function in Ball with Delta for variation & paddles for the collision
+		ball.updateAnimationFrame(delta, [paddleTop.rect, paddleBottom.rect]);
+		//Hue variation wrt Delta
+		const hue = parseFloat(
+			getComputedStyle(document.documentElement).getPropertyValue("--hue")
+		);
+		document.documentElement.style.setProperty("--hue", hue + delta * 0.01);
+		// If Lost the match?
+		if (lost()) {
+			//Game is OFF
+			ball.GAME_ON = false;
+			//Stop Animation
+			animation = false;
+		} else {
+			//Continue Game
+			ball.GAME_ON = true;
+			//Continue Animation
+			animation = true;
+		}
+	}
+	
+	if (!animation) {
+		window.cancelAnimationFrame(clearFrame);
+		matchEnds();
+		console.log("clearFrame1: ", clearFrame);
+		const localScore = localStorage.getItem("maxScore");
+		const localRod = localStorage.getItem("maxRod");
+		alert(
+			`${whoWon} wins with a score of ${score}\nMax Score is: ${localScore} by ${localRod}`
+		);
+	} else {
+		previousTime = currentTime;
+		clearFrame = window.requestAnimationFrame(updateAnimationFrame);
+		console.log("RUNNING....");
+	}
+};
+let lost = () => {
+	const ballRect = ball.rect;
+	//Ball goes beyond bottom
+	if (ballRect.bottom >= windowHeight) {
+		whoWon = "Rod 1";
+		ball.paddleTopScore += 100;
+	}
+	//Ball goes beyond top
+	else if (ballRect.top <= 0) {
+		whoWon = "Rod 2";
+		ball.paddleBottomScore += 100;
+	}
+	return ballRect.bottom >= windowHeight || ballRect.top <= 0;
+};
+
+//Local Storage Function//
+let storeScore = (maxScore, score, whoWon) => {
+	if (maxScore < score) {
+		maxScore = score;
+		maxRod = whoWon;
+		//Storing in Local Storage
+		localStorage.setItem("maxScore", maxScore);
+		localStorage.setItem("maxRod", maxRod);
+		console.log(maxScore, score, whoWon);
+	}
+};
+
+let matchEnds = () => {
+	maxScore = localStorage.getItem("maxScore");
+	if (maxScore === null) {
+		maxScore = 0;
+	}
+	if (whoWon === "Rod 1") {
+		score = ball.paddleTopScore;
+	} else if (whoWon === "Rod 2") {
+		score = ball.paddleBottomScore;
+	}
+	//Store the score and reset everything else
+	storeScore(maxScore, score, whoWon);
+	ball.reset();
+	paddleTop.reset();
+	paddleBottom.reset();
+	ball.paddleTopScoreElement.innerText = 0;
+	ball.paddleBottomScoreElement.innerText = 0;
+};
+
+window.addEventListener("keypress", (event) => {
+	let paddlePositionX = paddleTop.positionX;
+	
+	if (event.keyCode === 100 && paddlePositionX < 86) {
+		paddleTop.positionX = paddlePositionX + 2;
+		paddleBottom.positionX = paddlePositionX + 2;
+	}
+	
+	else if (event.keyCode === 97 && paddlePositionX > 0) {
+		paddleTop.positionX = paddlePositionX - 2;
+		paddleBottom.positionX = paddlePositionX - 2;
+	}
+	//Start the Game & Move the Ball
+	else if (event.keyCode === 13 && ball.GAME_ON === false) {
+		
+		window.cancelAnimationFrame(clearFrame2);
+		console.log("clearFrame2: ", clearFrame2);
+		//Game ON
+		ball.GAME_ON = true;
+		//Previous Time is null, so that the animation loop starts from the very first beginning as it has only the current time at the start
+		previousTime = null;
+		//Animation ON
+		animation = true;
+		//Reset everything else
+		ball.reset();
+		paddleTop.reset();
+		paddleBottom.reset();
+		paddleTopScoreElement.innerText = 0;
+		paddleBottomScoreElement.innerText = 0;
+		clearFrame2 = window.requestAnimationFrame(updateAnimationFrame);
+	}
+});
+//--------------------------------------------------------------
+// <== ==> - Keydown Events//
+window.addEventListener("keydown", (event) => {
+	let paddlePositionX = paddleTop.positionX;
+	//Move the Paddle Right
+	if (event.keyCode === 39 && paddlePositionX < 86) {
+		paddleTop.positionX = paddlePositionX + 2;
+		paddleBottom.positionX = paddlePositionX + 2;
+	}
+	//Move the Paddle Left
+	else if (event.keyCode === 37 && paddlePositionX > 0) {
+		paddleTop.positionX = paddlePositionX - 2;
+		paddleBottom.positionX = paddlePositionX - 2;
+	}
+});
+//--------------------------------------------------------------
